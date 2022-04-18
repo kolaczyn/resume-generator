fa974e48c2e8a414be660c79c58ababa868a2d17
Paweł Kołaczyński <Pawel.Kolaczynski@rossmann.com.pl>
2022-04-18 14:48:05 +0200

    initial webpack setup

---
+++ README.md
@@ -0,0 +1,12 @@
+# Resume generator
+
+## How to run
+
+```sh
+yarn
+yarn build
+```
+
+Now open `dist/index.html` with your browser
+
+<!-- Now open with your browser on [localhost:3000](http://localhost:3000). -->
---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: "public", to: "." }],
+      options: {
+        concurrency: 100,
+      },
+    }),
+  ],
+};
